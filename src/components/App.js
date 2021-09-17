6ed94bc5585503bb4a4a8f223641d256d39b5aff
Yogesh Rajesh  Phutane <88277315+theyogesh98@users.noreply.github.com>
2021-09-17 17:32:00 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/App.css';
+import Welcome from './Welcome';
+
+const App = () => {
+  return (
+    <Welcome name="Yogesh Rajesh Phutane" />
+  )
+}
+
+
+export default App;
